af9292ccc3c3b4d6f1a9711d371bedc7e02e2ef2
eggtgg <mtri.egg@gmail.com>
2021-01-08 11:23:16 +0700

    hello sửa code vui thôi

--- bt_1_numpy.py
+++ bt_1_numpy.py
@@ -2,27 +2,27 @@ import numpy as np
 from sympy import *
 
 
-def matrix(m, n):
-    A = np.random.randint(0, 10, size=n*m).reshape(m, n)
-    #print('ma trận a: \n', A)
-    return A
+def matrix(n, m):
+    a = np.random.randint(0, 10, size=n*m).reshape(n, m)
+    print('ma trận a: \n', a)
+    return a
 
 
-def rank_matran(A):
-    rank_A = np.linalg.matrix_rank(A)
-    #print(f'rank = {rank_A}')
-    return rank_A
+def rank_matran(a):
+    rank_a = np.linalg.matrix_rank(a)
+    print(f'rank = {rank_a}')
+    return rank_a
 
 
-def bac_thang(A):
-    M = Matrix(A)
+def bac_thang(a):
+    M = Matrix(a)
     # Use sympy.rref() method
     M_rref = M.rref()
-    #print('Ma trận bậc thang: \n', M_rref)
-    return np.array(M_rref[0])
+    print("The Row echelon form of matrix M and the pivot columns : {}".format(M_rref))
 
 
 def main():
+<<<<<<< HEAD
     a = matrix(3,4)
     r = rank_matran(a)
     b = bac_thang(a)
@@ -31,6 +31,12 @@ def main():
     print('helo tất cả các bạn. Mình là Minh Trí Hồ')
     print("Mình sẽ test file này cho các bạn xem")
     print("Vui là chính vì tau đã sửa file")
+=======
+    b = matrix(4,5)
+    rank_matran(b)
+    bac_thang(b)
+
+>>>>>>> parent of 8321e59... final code
 
 if __name__ == '__main__':
      main()
